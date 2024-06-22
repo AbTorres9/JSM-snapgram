a51b45fa7c3eb8d7bf25bd1f674e374338daa196
Abhishek Barthwal <abhi.barthwal9@gmail.com>
2024-06-22 14:20:16 +0530

    22june

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+// import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./globals.css";
+import Signinform from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <main className="flex h-screen">
+        <Routes>
+          {/* public routes */}
+          <Route element={<AuthLayout />}>
+            <Route path="/sign-in" element={<Signinform />} />
+            <Route path="/sign-up" element={<SignupForm />} />
+          </Route>
+
+          {/* private routes */}
+          <Route element={<RootLayout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </main>
+    </>
+  );
+}
+
+export default App;
